41713e139dee60e87e6f9efddd1d35a3456d9d91
André Luís Pessetti <pessetti@gmail.com>
2019-09-26 13:24:03 -0300

    Signup

--- index.php
+++ index.php
@@ -26,36 +26,13 @@ if (!empty($_POST['loginSubmit'])) {
         }
     }
 }
-
-if (!empty($_POST['signupSubmit'])) {
-
-    $email = $_POST['emailReg'];
-    $password = $_POST['passwordReg'];
-    $name = $_POST['nameReg'];
-    $email_check = preg_match('~^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.([a-zA-Z]{2,4})$~i', $email);
-    $password_check = preg_match('~^[A-Za-z0-9!@#$%^&*()_]{6,20}$~i', $password);
-
-    if ($email_check && $password_check && strlen(trim($name)) > 0) {
-
-        $uid = $userClass->userRegistration($password, $email, $name, $secret);
-        if ($uid) {
-            $url = BASE_URL . 'device_confirmations.php';
-            header("Location: $url");
-        } else {
-            $errorMsgReg = "Este Usuário ou E-mail já existe.";
-        }
-    } else {
-        $errorMsgReg = "Insira Dados Válidos";
-    }
-}
-
 ?>
 <!DOCTYPE html>
 <html>
 
 <head>
     <title>Banco de Questões - SENAI/CTAI</title>
-    <link rel="stylesheet" type="text/css" href="style.css" charset="utf-8" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" charset="utf-8" />
 </head>
 
 <body>
@@ -70,21 +47,6 @@ if (!empty($_POST['signupSubmit'])) {
                 <input type="password" name="password" autocomplete="off" />
                 <div class="errorMsg"><?php echo $errorMsgLogin; ?></div>
                 <input type="submit" class="button" name="loginSubmit" value="Login">
-            </form>
-        </div>
-
-
-        <div id="signup">
-            <h3>Cadastro</h3>
-            <form method="post" action="" name="signup">
-                <label>Nome</label>
-                <input type="text" name="nameReg" autocomplete="off" />
-                <label>Email</label>
-                <input type="email" name="emailReg" autocomplete="off" />
-                <label>Senha</label>
-                <input type="password" name="passwordReg" autocomplete="off" />
-                <div class="errorMsg"><?php echo $errorMsgReg; ?></div>
-                <input type="submit" class="button" name="signupSubmit" value="Cadastrar">
             </form>
         </div>
 
